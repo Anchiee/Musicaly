20f4aac6d04edcaf07a288f52782cc04aa93520f
Anchiee <wyhwtf@gmail.com>
2025-04-14 22:50:48 +0200

    added animations

--- resources/js/Layouts/AuthLayout.tsx
+++ resources/js/Layouts/AuthLayout.tsx
@@ -1,37 +1,38 @@
 import { LayoutProps } from "@/types/layout"
 import Header from "@/Components/Header"
 import { Head } from "@inertiajs/react"
-
+import AnimatedPage from "@/Components/Animations/AnimatedPage"
 
 export default function AuthLayout({children, title} : LayoutProps) {
     return(
         <main className="size-full flex">
-
             <Head title={title}/>
-            {children}
+
+            <AnimatedPage className="size-full flex"> 
+                {children}
 
                 <section className="bg-violet w-full">
-                <div className="flex justify-center items-center h-full">
-                    <div className="bg-white px-5 py-10 rounded-md shadow-xl">
-                        <img src="Musicaly2.png" alt="Musicaly logo" className="size-50 mx-auto"/>
-                        <Header text="Dive into a world of music"/>
-                        <p className="text-[.9rem]"> No ads, no limits - just pure, high-quality sound. Stream your favorite songs anytime, anywhere.</p>
+                    <div className="flex justify-center items-center h-full">
+                        <div className="bg-white px-5 py-10 rounded-md shadow-xl">
+                            <img src="Musicaly2.png" alt="Musicaly logo" className="size-50 mx-auto"/>
+                            <Header text="Dive into a world of music"/>
+                            <p className="text-[.9rem]"> No ads, no limits - just pure, high-quality sound. Stream your favorite songs anytime, anywhere.</p>
 
-                        <div className="my-3 py-3 border-t-[1px] border-b-[1px] border-gray-200">
-                            <Header text="we offer" level={2}/>
-                            <ul className="text-[.9rem]">
-                                <li className="my-1">Unlimited skips</li>
-                                <li className="my-1">High-fidelity audio</li>
-                                <li className="my-1">Personalized playlists</li>
-                            </ul>
+                            <div className="my-3 py-3 border-t-[1px] border-b-[1px] border-gray-200">
+                                <Header text="we offer" level={2}/>
+                                <ul className="text-[.9rem]">
+                                    <li className="my-1">Unlimited skips</li>
+                                    <li className="my-1">High-fidelity audio</li>
+                                    <li className="my-1">Personalized playlists</li>
+                                </ul>
+                            </div>
+                            
+                            <p className="text-[.9rem] mt-5">© {new Date().getFullYear()} Musicaly — Open Source under MIT License</p>
+
                         </div>
-                        
-                        <p className="text-[.9rem] mt-5">© {new Date().getFullYear()} Musicaly — Open Source under MIT License</p>
-
                     </div>
-
-                </div>
-            </section>
+                </section>
+            </AnimatedPage>
         </main>   
     )
 }
