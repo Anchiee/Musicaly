87af0dd32191e4f17fe2275f4d345fe5faea0c39
Anchiee <wyhwtf@gmail.com>
2025-04-14 21:55:52 +0200

    button redirect to login page

--- resources/js/pages/Home.tsx
+++ resources/js/pages/Home.tsx
@@ -1,6 +1,8 @@
 import MainLayout from "@/Layouts/MainLayout";
 import AnimatedPage from "@/Components/Animations/AnimatedPage";
 import Header from "@/Components/Header";
+import { Link } from "@inertiajs/react";
+
 
 export default function Home() {
 
@@ -15,11 +17,11 @@ export default function Home() {
                             Whether it's the beat that lifts you up or the lyrics that understand you - Musicaly is built to bring you closer to the songs you love. It's more than streaming. It's connection.
                         </p>
 
-                        <button 
+                        <Link as="button" href="/login"
                         className="mt-3 border-2 border-violet font-semibold cursor-pointer text-lg pl-7 pr-8 py-2 rounded-full text-violet 
                         hover:text-white hover:bg-violet transition-colors">
                             Try it
-                        </button>
+                        </Link>
                     
                     </div>
                 
