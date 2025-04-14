f0d9521ece159bfa469a7c25f80e0576abe5b374
Anchiee <wyhwtf@gmail.com>
2025-04-14 22:03:17 +0200

    login page improvement

--- resources/js/Components/Input.tsx
+++ resources/js/Components/Input.tsx
@@ -6,5 +6,5 @@ type InputProps = {
 }
 
 export default function Input({type, placeholder, id} : InputProps) {
-    return <input type={type} placeholder={placeholder} id={id} className="block p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500"/>
+    return <input type={type} placeholder={placeholder} id={id} className="block p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:outline-violet"/>
 }
