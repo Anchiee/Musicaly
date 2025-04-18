1851f55411722e660a410da462557225c037a472
Anchiee <wyhwtf@gmail.com>
2025-04-19 00:06:11 +0200

    verify email frontend page

--- resources/js/Components/SolidButton.tsx
+++ resources/js/Components/SolidButton.tsx
@@ -5,5 +5,5 @@ type ButtonProps = {
 }
 
 export default function SolidButton({text, type} : ButtonProps) {
-    return <button type={type} className="bg-violet text-white py-2 rounded-md cursor-pointer hover:opacity-80 transition-opacity">{text}</button>
+    return <button type={type} className="bg-violet text-white py-2 rounded-md cursor-pointer hover:opacity-80 transition-opacity w-full">{text}</button>
 }
